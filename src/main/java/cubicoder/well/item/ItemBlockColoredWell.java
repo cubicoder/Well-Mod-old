e553071d07bbb1fd2b73cea254f8e4f1317ac6b3
cubicoder <40398183+cubicoder@users.noreply.github.com>
2023-01-17 22:09:30 +0000

    ItemBlockColoredWell ported

--- src/main/java/cubicoder/well/item/ItemBlockColoredWell.java
+++ src/main/java/cubicoder/well/item/ItemBlockColoredWell.java
@@ -1,49 +1,59 @@
 package cubicoder.well.item;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockCauldron;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LayeredCauldronBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.items.ItemHandlerHelper;
 
-import javax.annotation.Nonnull;
+public class ItemBlockColoredWell extends BlockItem {
 
-/**
- *
- * @author jbred
- *
- */
-public class ItemBlockColoredWell extends ItemBlock
-{
-    public ItemBlockColoredWell(@Nonnull Block block) { super(block); }
+	public ItemBlockColoredWell(Block block, Properties properties) {
+		super(block, properties);
+	}
+	
+	@Override
+	public InteractionResult useOn(UseOnContext context) {
+		BlockState state = context.getLevel().getBlockState(context.getClickedPos());
+		if (state.getBlock() instanceof LayeredCauldronBlock) {
+			if (!context.getLevel().isClientSide && !context.getPlayer().isCreative()) {
+				int level = state.getValue(LayeredCauldronBlock.LEVEL);
+				if (level > 0) {
+					context.getPlayer().getItemInHand(context.getHand()).shrink(1);
+					context.getPlayer().awardStat(Stats.USE_CAULDRON);
+					ItemHandlerHelper.giveItemToPlayer(context.getPlayer(), new ItemStack(ModItems.WELL));
+					LayeredCauldronBlock.lowerFillLevel(state, context.getLevel(), context.getClickedPos());
+				}
+			}
+		}
+		
+		return InteractionResult.SUCCESS;
+	}
+	
+	/*@Nonnull
+	@Override
+	public EnumActionResult onItemUse(@Nonnull EntityPlayer player, @Nonnull World worldIn, @Nonnull BlockPos pos,
+			@Nonnull EnumHand hand, @Nonnull EnumFacing facing, float hitX, float hitY, float hitZ) {
+		final IBlockState state = worldIn.getBlockState(pos);
+		if (state.getBlock() instanceof BlockCauldron) {
+			if (!worldIn.isRemote && !player.isCreative()) {
+				final int level = state.getValue(BlockCauldron.LEVEL);
+				if (level > 0) {
+					player.getHeldItem(hand).shrink(1);
+					player.addStat(StatList.CAULDRON_USED);
+					ItemHandlerHelper.giveItemToPlayer(player, new ItemStack(ModItems.WELL));
+					((BlockCauldron) state.getBlock()).setWaterLevel(worldIn, pos, state, level - 1);
 
-    @Nonnull
-    @Override
-    public EnumActionResult onItemUse(@Nonnull EntityPlayer player, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull EnumHand hand, @Nonnull EnumFacing facing, float hitX, float hitY, float hitZ) {
-        final IBlockState state = worldIn.getBlockState(pos);
-        if(state.getBlock() instanceof BlockCauldron) {
-            if(!worldIn.isRemote && !player.isCreative()) {
-                final int level = state.getValue(BlockCauldron.LEVEL);
-                if(level > 0) {
-                    player.getHeldItem(hand).shrink(1);
-                    player.addStat(StatList.CAULDRON_USED);
-                    ItemHandlerHelper.giveItemToPlayer(player, new ItemStack(ModItems.WELL));
-                    ((BlockCauldron)state.getBlock()).setWaterLevel(worldIn, pos, state, level - 1);
+				}
+			}
 
-                }
-            }
+			return EnumActionResult.SUCCESS;
+		}
 
-            return EnumActionResult.SUCCESS;
-        }
-
-        return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
-    }
+		return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+	}*/
 }
--- src/main/java/cubicoder/well/item/ModItems.java
+++ src/main/java/cubicoder/well/item/ModItems.java
@@ -1,14 +1,13 @@
 package cubicoder.well.item;
 
-import net.minecraft.item.EnumDyeColor;
-import net.minecraft.item.ItemBlock;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
 import cubicoder.well.block.ModBlocks;
-
-import java.util.ArrayList;
-import java.util.List;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.world.item.BlockItem;
 
 /**
  *
@@ -17,38 +16,38 @@ import java.util.List;
  */
 public final class ModItems
 {
-    @Nonnull public static final List<ItemBlock> INIT = new ArrayList<>();
+    @Nonnull public static final List<BlockItem> INIT = new ArrayList<>();
 
-    @Nonnull public static final ItemBlock WELL = register("well", new ItemBlock(ModBlocks.WELL));
-    @Nonnull public static final ItemBlock WHITE_WELL      = register(0);
-    @Nonnull public static final ItemBlock ORANGE_WELL     = register(1);
-    @Nonnull public static final ItemBlock MAGENTA_WELL    = register(2);
-    @Nonnull public static final ItemBlock LIGHT_BLUE_WELL = register(3);
-    @Nonnull public static final ItemBlock YELLOW_WELL     = register(4);
-    @Nonnull public static final ItemBlock LIME_WELL       = register(5);
-    @Nonnull public static final ItemBlock PINK_WELL       = register(6);
-    @Nonnull public static final ItemBlock GRAY_WELL       = register(7);
-    @Nonnull public static final ItemBlock SILVER_WELL     = register(8);
-    @Nonnull public static final ItemBlock CYAN_WELL       = register(9);
-    @Nonnull public static final ItemBlock PURPLE_WELL     = register(10);
-    @Nonnull public static final ItemBlock BLUE_WELL       = register(11);
-    @Nonnull public static final ItemBlock BROWN_WELL      = register(12);
-    @Nonnull public static final ItemBlock GREEN_WELL      = register(13);
-    @Nonnull public static final ItemBlock RED_WELL        = register(14);
-    @Nonnull public static final ItemBlock BLACK_WELL      = register(15);
+    @Nonnull public static final BlockItem WELL = register("well", new BlockItem(ModBlocks.WELL));
+    @Nonnull public static final BlockItem WHITE_WELL      = register(0);
+    @Nonnull public static final BlockItem ORANGE_WELL     = register(1);
+    @Nonnull public static final BlockItem MAGENTA_WELL    = register(2);
+    @Nonnull public static final BlockItem LIGHT_BLUE_WELL = register(3);
+    @Nonnull public static final BlockItem YELLOW_WELL     = register(4);
+    @Nonnull public static final BlockItem LIME_WELL       = register(5);
+    @Nonnull public static final BlockItem PINK_WELL       = register(6);
+    @Nonnull public static final BlockItem GRAY_WELL       = register(7);
+    @Nonnull public static final BlockItem SILVER_WELL     = register(8);
+    @Nonnull public static final BlockItem CYAN_WELL       = register(9);
+    @Nonnull public static final BlockItem PURPLE_WELL     = register(10);
+    @Nonnull public static final BlockItem BLUE_WELL       = register(11);
+    @Nonnull public static final BlockItem BROWN_WELL      = register(12);
+    @Nonnull public static final BlockItem GREEN_WELL      = register(13);
+    @Nonnull public static final BlockItem RED_WELL        = register(14);
+    @Nonnull public static final BlockItem BLACK_WELL      = register(15);
 
     @Nonnull
-    static ItemBlock register(@Nonnull String name, @Nonnull ItemBlock item) {
+    static BlockItem register(@Nonnull String name, @Nonnull BlockItem item) {
         item.setRegistryName("well", name).setTranslationKey("well." + name);
         INIT.add(item);
         return item;
     }
 
     @Nonnull
-    static ItemBlock register(int index) {
+    static BlockItem register(int index) {
         return register(
                 EnumDyeColor.byMetadata(index).getName() + "_well",
-                new ItemBlockColoredWell(ModBlocks.INIT.get(index + 1))
+                new BlockItemColoredWell(ModBlocks.INIT.get(index + 1))
         );
     }
 }
